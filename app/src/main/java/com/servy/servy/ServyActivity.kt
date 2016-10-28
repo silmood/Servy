830658b0dcb071a7dc7685077e99608ea49a15a7
Pedro Antonio <silmood@protonmail.com>
2016-10-27 20:32:35 -0500

    Initial commit

---
+++ app/src/main/java/com/servy/servy/ServyActivity.kt
@@ -0,0 +1,12 @@
+package com.servy.servy
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ServyActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_delivery_code)
+    }
+}
---
+++ app/src/main/java/com/servy/servy/view/HintResizeableEditText.kt
@@ -0,0 +1,47 @@
+package com.servy.servy.view
+
+import android.content.Context
+import android.content.res.TypedArray
+import android.support.v7.widget.AppCompatEditText
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.AttributeSet
+import android.view.inputmethod.InputMethodManager
+import com.servy.servy.R
+
+class HintResizeableEditText : AppCompatEditText{
+
+    private var hintTextSize : Int = 0
+    private var normalTextSize: Float = 0f
+
+    constructor(context: Context) : super(context) {
+        init(context, null)
+    }
+
+    constructor(context: Context, attributeSet: AttributeSet?) : super(context, attributeSet){
+        init(context, attributeSet)
+    }
+
+    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr){
+        init(context, attrs)
+    }
+
+
+    private fun init(context: Context, attributeSet: AttributeSet?) {
+       if(attributeSet == null)
+           return
+
+        val typedArray : TypedArray = context.obtainStyledAttributes(attributeSet, R.styleable.HintResizeableEditText)
+        hintTextSize = typedArray.getDimensionPixelSize(R.styleable.HintResizeableEditText_hintTextSize, context.resources.getDimensionPixelSize(R.dimen.default_hint_size))
+        normalTextSize = textSize
+        textSize = hintTextSize.toFloat()
+
+        setOnFocusChangeListener { view, hasFocus ->
+            textSize = if (!hasFocus && text.length == 0) hintTextSize.toFloat() else normalTextSize
+            val imm : InputMethodManager = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            imm.showSoftInput(this, InputMethodManager.SHOW_IMPLICIT)
+        }
+
+        typedArray.recycle()
+    }
+}
